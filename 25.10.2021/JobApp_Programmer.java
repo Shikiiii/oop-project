edfe5f3790fca1787e11b3c41f8ef634df8f183f
YouSimik <78479782+YouSimik@users.noreply.github.com>
2021-10-25 16:01:23 +0300

    Create JobApp_Programmer.java

---
+++ 25.10.2021/JobApp_Programmer.java
@@ -0,0 +1,20 @@
+package ООП_Проект_jobApplication;
+
+public class JobApp_Programmer {
+
+    String fullName;
+    int age;
+    String devType;  // web, game, sysadmin/devOPS
+    String language; // html, css, js, php
+    int experience;  // 15 години
+
+    public JobApp_Programmer(String fullName, int age, String devType, String language, int experience){
+        this.fullName = fullName;
+        this.age = age;
+        this.devType = devType;
+        this.language = language;
+        this.experience = experience;
+
+
+    }
+}
